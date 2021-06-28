386b039746f4cff4dcfacdf898926b31052e5d30
Vinicius D. Gubolin <vinicius.demite@outlook.com>
2021-06-28 20:59:09 -0300

    instalação do Vue, mathjs e componentização base

---
+++ resources/js/components/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+      <div class="row">
+          <div class="col-12">
+              <div class="card">
+                    <div class="card-header text-center">
+                        <h3>Calculator</h3>
+                    </div>
+
+                    <div class="card-body">
+                        <calculator-component v-model="formula"></calculator-component>
+                    </div>
+                </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                formula: '',
+                result: 0,
+            }
+        },
+
+        watch: {
+            formula: function(value) {
+                this.calculate(value)
+            }
+        },
+        
+        methods: {
+            calculate(formula) {
+                console.log(math.evaluate(formula))
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
+Calculator
---
+++ resources/js/components/Calculator.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="calculator">
+      <input type="text" name="formula" id="formula" class="form-control text-center calculator__formula" @change="formula($event.target.value)" />
+
+      <div class="calculator__result" v-if="result > 0">{{ result }}</div>
+
+      <div class="calculator__history"></div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'Calculator',
+
+        props: {
+            result: {
+                type: Float64Array,
+                require: true,
+            }
+        },
+
+        methods: {
+            formula(value) {
+                this.$emit('input', value)
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
